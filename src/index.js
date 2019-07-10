a5505e729b9e07a1fd80ac662550c2fbd209b862
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2019-07-10 11:56:31 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie9'; // For IE 9-11 support
 import 'react-app-polyfill/stable';
 // import 'react-app-polyfill/ie11'; // For IE 11 support
-import './utilities/polyfill';
+import './polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import { App } from './bootstrap';
-import * as serviceWorker from './utilities/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
